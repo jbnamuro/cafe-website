ed8d2e1a94faef910d10d12dde65e3310ade3148
jbnamuro <jbnamuro12@gmail.com>
2025-07-06 16:04:03 -0400

    changes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <h2>Corner Coffee</h2>
+    <div className="mt-10 ml-5 pb-10">
+      <h2 className="text-7xl font-bold tracking-tighter mb-20">
+        Corner Coffee
+      </h2>
       <div>
         <p>Menu</p>
         <p>Home</p>
@@ -13,19 +15,34 @@ const Footer = () => {
       <div>
         <p>Full Menu</p>
       </div>
-      <div>
-        <a href="" target="_blank">
-          INSTAGRAM
-        </a>
-        <a href="" target="_blank">
-          FACEBOOK
-        </a>
-        <a href="" target="_blank">
-          EMAIL
-        </a>
-        <a href="" target="_blank">
-          TIKTOK
-        </a>
+      <div className="mt-20">
+        <p className="font-bold mb-2 tracking-tighter">FOLLOW US</p>
+        <div className="flex gap-5 font-medium">
+          <div className="bg-ccgreen flex gap-2 text-cream px-2 py-2 rounded-md">
+            <a href="" target="_blank" className="">
+              INSTAGRAM
+            </a>
+            <p>↗</p>
+          </div>
+          <div className="bg-ccgreen flex gap-2 text-cream px-2 py-2 rounded-md">
+            <a href="" target="_blank">
+              FACEBOOK
+            </a>
+            <p>↗</p>
+          </div>
+          <div className="bg-ccgreen flex gap-2 text-cream px-2 py-2 rounded-md">
+            <a href="" target="_blank">
+              EMAIL
+            </a>
+            <p>↗</p>
+          </div>
+          <div className="bg-ccgreen flex gap-2 text-cream px-2 py-2 rounded-md">
+            <a href="" target="_blank">
+              TIKTOK
+            </a>
+            <p>↗</p>
+          </div>
+        </div>
       </div>
     </div>
   );
